d0e9da0d96d749de0d91902887ef035e6cc551b7
JieZhan <178889339@qq.com>
2021-11-27 15:44:24 +0800

    Topnav组件封装

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,8 @@
 <template>
-  <div>
-    导航栏 |
-    <router-link to="/switch">switch</router-link>|
-    <router-link to="/button">button</router-link>
-  </div>
   <router-view />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App"
 };
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import { createWebHashHistory, createRouter } from 'vue-router'
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
 
 const history = createWebHashHistory()
 const router = createRouter({
     history: history,
     routes: [
+        { path: '/', component: Home },
+        { path: '/Doc', component: Doc },
         { path: '/switch', component: '' },
         { path: '/button', component: '' },
     ]
