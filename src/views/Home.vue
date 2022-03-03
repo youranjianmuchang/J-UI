a01c04220db861139d2afd49e1553e72bbe5a62a
JieZhan <178889339@qq.com>
2022-03-03 21:40:04 +0800

    优化首页

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,40 @@
 <template>
   <Topnav />
   <div class="banner">
-    <h1 class="topic">J-UI</h1>
+    <h1 class="topic">
+    <svg class="logo">
+      <use xlink:href="#icon-J"></use>
+    </svg>-UI</h1>
     <h2>一个简单好用的ui框架</h2>
     <p class="actions">
-      <a class="github">Github</a>
-      <router-link class="start" to="/Doc">开始使用</router-link>
+      <a class="github" href="https://github.com/youranjianmuchang/J-UI">Github</a>
+      <router-link class="start" to="/Doc/Install">开始使用</router-link>
     </p>
+  </div>
+  <div class="features">
+    <ul>
+      <li>
+        <svg>
+          <use xlink:href="#icon-vue"></use>
+        </svg>
+        <h3>基于 Vue 3</h3>
+        <p>使用了 Vue 3 Composition API</p>
+      </li>
+      <li>
+        <svg>
+          <use xlink:href="#icon-ts"></use>
+        </svg>
+        <h3>基于 TypeScript </h3>
+        <p>源代码采用 TypeScript 书写</p>
+      </li>
+      <li>
+        <svg>
+          <use xlink:href="#icon-light"></use>
+        </svg>
+        <h3>代码易读</h3>
+        <p>每个组件的源代码都极其简洁</p>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -16,7 +44,7 @@ export default {
   name: "Home.vue",
   components: {
     Topnav
-  }
+  },
 };
 </script>
 
@@ -31,6 +59,12 @@ export default {
   > .topic {
     font-size: 3.5em;
     margin-bottom: 30px;
+    .logo{
+      width: 50px;
+      height: 50px;
+      position: relative;
+      top: 5px;
+    }
   }
   > .actions {
     margin-top: 30px;
@@ -49,6 +83,53 @@ export default {
     .github {
       color: #0170fe;
       border: 1px solid #0170fe;
+    }
+  }
+}
+.features {
+  margin: 64px auto;
+  padding: 0 50px;
+  @media (min-width: 800px) {
+    width: 800px;
+    >ul {
+      >li {
+        width: 50%;
+      }
+    }
+  }
+  @media (min-width: 1200px) {
+    width: 1200px;
+    >ul {
+      >li {
+        width: 33.3333%;
+      }
+    }
+  }
+  >ul {
+    display: flex;
+    flex-wrap: wrap;
+    >li {
+      margin: 16px 0;
+      display: grid;
+      justify-content: start;
+      align-content: space-between;
+      grid-template-areas:
+              "icon title"
+              "icon text";
+      grid-template-columns: 80px auto;
+      grid-template-rows: 1fr auto;
+      >svg {
+        grid-area: icon;
+        width: 64px;
+        height: 64px;
+      }
+      >h3 {
+        grid-area: title;
+        font-size: 28px;
+      }
+      >p {
+        grid-area: text
+      }
     }
   }
 }
